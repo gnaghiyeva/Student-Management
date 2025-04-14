3510d62aa6ede9e68028649d8556f83f9ca2671f
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2025-04-14 10:46:41 +0400

    update student

--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -1,8 +1,10 @@
 package service;
 
+import helper.InputHelper;
 import model.Student;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class StudentService {
@@ -34,5 +36,37 @@ public class StudentService {
             System.out.println("Bu ID-li tələbə yoxdur.");
             return false;
         }
+    }
+
+    public Student getStudentByID(int id){
+        for(Student student:students){
+            if(student.getId()==id){
+                return student;
+            }
+        }
+        return null;
+    }
+    
+    public boolean updateStudent(int id){
+        Student student = getStudentByID(id);
+        if (student==null){
+            System.out.println("Bu ID-li tələbə tapılmadı.");
+            return false;
+        }
+        String name = InputHelper.readText("Yeni ad:");
+        String surname = InputHelper.readText("Yeni soyad");
+        int age = InputHelper.readNumber("Yeni yaş");
+        List<String> subjects = Collections.singletonList(InputHelper.readText("Yeni fənn"));
+        double gpa = InputHelper.readNumber("Yeni gpa");
+
+        student.setName(name);
+        student.setSurname(surname);
+        student.setAge(age);
+        student.setSubjects(subjects);
+        student.setGpa(gpa);
+        System.out.println("Tələbə məlumatları aşağıdakı kimi güncəlləndi" +
+                student.getName()+" "+student.getSurname() +"-" +student.getAge() +" // "+"\n"+
+               "Fənnlər: "+ student.getSubjects()+" "+ "GPA: "+student.getGpa());
+        return true;
     }
 }
